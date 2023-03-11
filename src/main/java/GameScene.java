c2b5afce42047c067ee4b599e4a3ef50820e7d8f
OkO2451 <owyszwin@gmail.com>
2023-03-11 17:08:13 +0100

    Added backgound and foreground
    todo get the snakc

--- src/main/java/GameScene.java
+++ src/main/java/GameScene.java
@@ -1,13 +1,17 @@
-import java.awt.Graphics;
-import java.awt.Color;
+import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.geom.Rectangle2D;
 
 public class GameScene extends Scene{
+
+    Rect foreground, background;
     public Kl keyListener ;
     public Ml mouseListener;
     public GameScene(Kl keyListener, Ml mouseListener) {
         this.keyListener = keyListener;
         this.mouseListener = mouseListener;
+        background = new Rect(0, 0, Constants.WIDTH, Constants.HEIGHT);
+        foreground = new Rect(24,48, 24*31, 24*22);
     }
     public void update(double dt) {
         if(keyListener.isPressed(KeyEvent.VK_UP)){
@@ -17,7 +21,12 @@ public class GameScene extends Scene{
         }
     }
     public void draw(Graphics g) {
-        g.setColor(Color.GREEN);
-        g.fillRect(0, 0, Constants.WIDTH, Constants.HEIGHT);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.GREEN);
+        g2d.fill(new Rectangle2D.Double(background.x, background.y, background.w, background.h));
+
+        g2d.setColor(Color.BLUE);
+        g2d.fill(new Rectangle2D.Double(foreground.x, foreground.y, foreground.w, foreground.h));
+
     }
 }
